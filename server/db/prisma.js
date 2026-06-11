7dd9fd86e0c0e11bc179791b9f270c865a7cd0c0
tatyanaa05 <111276242+tatyanaa05@users.noreply.github.com>
2026-06-11 15:01:13 +0300

    Update prisma.js

--- server/db/prisma.js
+++ server/db/prisma.js
@@ -1,5 +1,5 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
+
+export default prisma;
